c3f8f572c283865e4283039b2e604473afc21707
Quellenni <quellennibarreto@gmail.com>
2021-08-22 22:29:14 -0300

    Developed unit tests for create person

---
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -0,0 +1,47 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "xxxxxxx";
+    private static final String LAST_NAME = "xxxxxxx";
+    private static final String CPF_NUMBER = "123.456.789-10";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2008, 05, 7);
+
+    public static PersonDTO createFakeDTO() {
+
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("06-06-2007")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+        
+    public static Person createFakeEntity() {
+
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+            
+    }
+        
+        
+}
+
+
+
+
+
